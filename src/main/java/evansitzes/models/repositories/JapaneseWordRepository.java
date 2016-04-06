efe531471b629f76413d488cc97c7c14afdac8b2
Evan <evan@datarank.com>
2016-04-06 18:31:38 -0500

    refactor out system variables

--- src/main/java/evansitzes/models/repositories/JapaneseWordRepository.java
+++ src/main/java/evansitzes/models/repositories/JapaneseWordRepository.java
@@ -4,13 +4,12 @@ import evansitzes.models.entities.JapaneseWordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Created by evan on 4/6/16.
  */
-@Transactional
+
 public interface JapaneseWordRepository extends JpaRepository<JapaneseWordEntity, Long> {
 
     @Query("select j from JapaneseWordEntity j where j.active = true")
--- src/main/java/evansitzes/models/repositories/KoreanWordRepository.java
+++ src/main/java/evansitzes/models/repositories/KoreanWordRepository.java
@@ -4,14 +4,12 @@ import evansitzes.models.entities.KoreanWordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Created by evan on 4/6/16.
  */
 
-@Transactional
 public interface KoreanWordRepository extends JpaRepository<KoreanWordEntity, Long> {
 
     @Query("select k from KoreanWordEntity k where k.active = true")
